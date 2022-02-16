6deeff73f12417d8068972f1790bad4f80e9921b
Ahadu Kebede <ahadukebede@gmail.com>
2022-02-16 15:23:54 -0500

    Added `Header` Component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="header-container">
+		<img class="header-logo" alt="Vue logo" src="../assets/logo.png" />
+		<h2 class="title">{{ text }}</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Header",
+	props: {
+		text: String,
+	},
+};
+</script>
+
+<style scoped>
+.header {
+	margin: 2rem;
+	display: flex;
+	justify-content: space-evenly;
+	flex-wrap: nowrap;
+}
+.header-logo {
+	width: 7rem;
+	height: 7rem;
+}
+.title {
+}
+</style>
