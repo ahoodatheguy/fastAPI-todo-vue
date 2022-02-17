e00ac458b776456293006363a646ebfd32566aef
Ahadu Kebede <ahadukebede@gmail.com>
2022-02-17 17:53:06 -0500

    Changed CORS settings for frontend

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from serializers import SQL, post_bodies
 import json
 
 app = FastAPI()
 database = SQL.DataBase('tasks.sqlite3')
+
+origins = ['*']
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=['*'],
+	allow_headers=['*']
+)
 
 
 @app.get('/')
