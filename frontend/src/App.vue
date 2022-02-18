8420e688c8e1d5208557552d3e2a98470bc57671
Ahadu Kebede <ahadukebede@gmail.com>
2022-02-18 08:26:54 -0500

    Added "New Task" button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app">
 		<Header text="FastAPI TODO App" />
+		<Button text="New Task" :onClick="createTask" />
 		<Tasks />
 	</div>
 </template>
@@ -8,12 +9,32 @@
 <script>
 import Header from "./components/Header.vue";
 import Tasks from "./components/Tasks.vue";
+import Button from "./components/Button.vue";
 
 export default {
 	name: "App",
 	components: {
 		Header,
 		Tasks,
+		Button,
+	},
+	methods: {
+		createTask() {
+			// TODO: Create a component that handles user input, instead of using prompt();
+			let newTask = {
+				name: prompt("Task name"),
+				desc: prompt("Task Description"),
+			};
+			fetch("http://localhost:8000/create-task", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newTask),
+			});
+			// TODO: Access fetchTasks() from the Tasks component and use it here to avoid reloads.
+			location.reload();
+		},
 	},
 };
 </script>
---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,36 @@
+<template>
+	<button @click="onClick">{{ text }}</button>
+</template>
+
+<script>
+export default {
+	name: "Button",
+	props: {
+		text: String,
+		onClick: Function,
+	},
+};
+</script>
+
+<style scoped>
+button {
+	font-size: 1.2rem;
+	padding: 6px 12px;
+	margin-bottom: 0;
+
+	display: inline-block;
+	text-decoration: none;
+	text-align: center;
+	white-space: nowrap;
+	vertical-align: middle;
+	-ms-touch-action: manipulation;
+	touch-action: manipulation;
+	cursor: pointer;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+	background-image: none;
+	border: 1px solid transparent;
+}
+</style>
